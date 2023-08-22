75c914d1b55e45e70d56f3ed5d3a37d025fc3942
Victor <kolgatin@gmail.com>
2023-08-23 02:15:52 +0300

    create project

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from catalog.views import contacts, home
+from catalog.views import contacts, home, index
 
 urlpatterns = [
     path('', home),
-    path('contacts/', contacts)
+    path('contacts/', contacts),
+    path('', index)
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 # Create your views here.
+
+def index(request):
+    product_list = Product.objects.all()
+    context = {
+        'object_list':product_list
+    }
+    return render(request, 'catalog/index.html', context)
+
+
 def contacts(request):
     return render(request, 'catalog/contacts.html')
 
